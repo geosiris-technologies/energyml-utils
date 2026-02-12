7eaa43c7fa54adeca8909deceb33fa22f64c66dc
Valentin Gauthier <valentin.gauthier@geosiris.com>
2026-02-12 16:38:04 +0100

    bugfix for epcFile rels computation. Now support proxy rels type in resqml 201

--- src/main/java/com/geosiris/energyml/pkg/EPCPackage.java
+++ src/main/java/com/geosiris/energyml/pkg/EPCPackage.java
@@ -50,7 +50,8 @@ public class EPCPackage {
 
     protected final String domain;
     /**
-     * Version of the package : for resqml2_2 is '2.2', for resqml_dev3x_2_2 is '2.2dev3'
+     * Version of the package : for resqml2_2 is '2.2', for resqml_dev3x_2_2 is
+     * '2.2dev3'
      */
     protected final String domainVersion;
     protected final String versionNum;
@@ -58,27 +59,28 @@ public class EPCPackage {
     protected final String packageName;
     protected final String packagePath;
 
-
     public EPCPackage(String pkgPath, String xsdMappingFilePath) throws EPCPackageInitializationException {
         this(pkgPath, xsdMappingFilePath, Thread.currentThread().getContextClassLoader());
     }
 
-    public EPCPackage(String pkgPath, String xsdMappingFilePath, final ClassLoader classLoader) throws EPCPackageInitializationException {
+    public EPCPackage(String pkgPath, String xsdMappingFilePath, final ClassLoader classLoader)
+            throws EPCPackageInitializationException {
         String devVersionNum1;
         this.packagePath = pkgPath;
 
         Matcher pkgMatch = EPCGenericManager.PATTERN_ENERGYML_CLASS_NAME.matcher(pkgPath);
-        if(pkgMatch.find()){
+        if (pkgMatch.find()) {
             this.domain = pkgMatch.group("domain");
-            this.domainVersion = (pkgMatch.group("versionNum") + (pkgMatch.group("dev") != null ? pkgMatch.group("dev"): "")).replace("_", ".");
+            this.domainVersion = (pkgMatch.group("versionNum")
+                    + (pkgMatch.group("dev") != null ? pkgMatch.group("dev") : "")).replace("_", ".");
             this.packageName = pkgMatch.group("packageName");
             this.versionNum = pkgMatch.group("versionNum").replace("_", ".");
             try {
                 devVersionNum1 = pkgMatch.group("devNum").replace("_", ".");
-            }catch (Exception ignore){
+            } catch (Exception ignore) {
                 devVersionNum1 = null;
             }
-        }else{
+        } else {
             throw new EPCPackageInitializationException(pkgPath);
         }
 
@@ -87,27 +89,30 @@ public class EPCPackage {
         this.pkgClasses = new ArrayList<>(ContextBuilder.getClasses(this.packagePath, classLoader));
 
         SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        try{
+        try {
             xsdSchema = sf.newSchema(new File(Objects.requireNonNull(xsdMappingFilePath)));
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("ERR: no xsd schema for package '" + packagePath + "' at path " + xsdMappingFilePath);
         }
     }
 
-    public boolean isDevVersion(){
+    public boolean isDevVersion() {
         return devVersionNum != null;
     }
 
-    public boolean isReleaseVersion(){
-        return ! isDevVersion();
+    public boolean isReleaseVersion() {
+        return !isDevVersion();
     }
 
     public List<Class<?>> getRootsElementsClasses() {
-        // return pkgClasses.stream().filter(EPCGenericManager::isRootClass).collect(Collectors.toList()); OLD Way, but it includes types with no "createMY_TYPE_NAME(MY_TYPE value) -> JaxbElement" functions
+        // return
+        // pkgClasses.stream().filter(EPCGenericManager::isRootClass).collect(Collectors.toList());
+        // OLD Way, but it includes types with no "createMY_TYPE_NAME(MY_TYPE value) ->
+        // JaxbElement" functions
         Object objFactory = getObjectFactory();
         return Arrays.stream(objFactory.getClass().getMethods())
                 .filter(m -> m.getReturnType() == JAXBElement.class && m.getParameterCount() == 1)
-                .map(m->m.getParameters()[0].getType())
+                .map(m -> m.getParameters()[0].getType())
                 .collect(Collectors.toList());
     }
 
@@ -161,13 +166,13 @@ public class EPCPackage {
         long ticBegin = System.currentTimeMillis();
 
         logger.debug(">Trying to parse from package '" + this.packagePath);
-        
-        JAXBElement<?> result =  parseXmlFromContext(xmlContent, true, alternateDevVersionExists, xsdSchema);
+
+        JAXBElement<?> result = parseXmlFromContext(xmlContent, true, alternateDevVersionExists, xsdSchema);
         if (result != null) {
             logger.debug("Success reading with '" + this.packagePath + "' object class : "
                     + result.getValue().getClass().getName());
         } else {
-            logger.debug("\terror reading with package " + this.packagePath);  // + " --> " + xmlContent);
+            logger.debug("\terror reading with package " + this.packagePath); // + " --> " + xmlContent);
         }
 
         long ticEnd = System.currentTimeMillis();
@@ -176,20 +181,21 @@ public class EPCPackage {
     }
 
     private JAXBElement<?> parseXmlFromContext(String xmlContent,
-                                               boolean tryWithoutNamespaceIfFail,
-                                               boolean testRemarshalling, // test to marshall/unmarshall after first unmarshall
-                                               // to verify if everything has been read correctly
-                                               Schema schema) {
+            boolean tryWithoutNamespaceIfFail,
+            boolean testRemarshalling, // test to marshall/unmarshall after first unmarshall
+            // to verify if everything has been read correctly
+            Schema schema) {
         ValidationEventCollector vec = null;
 
         try {
             long ticCreateUnmarshaller_b = System.currentTimeMillis();
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            if(schema != null)
+            if (schema != null)
                 unmarshaller.setSchema(schema);
 
             long ticCreateUnmarshaller_e = System.currentTimeMillis();
-            logger.debug("\t@parseXmlFromContext (" + this.packagePath + ": Creating unmarshaller " + (ticCreateUnmarshaller_e - ticCreateUnmarshaller_b) / 1000.0 + "s");
+            logger.debug("\t@parseXmlFromContext (" + this.packagePath + ": Creating unmarshaller "
+                    + (ticCreateUnmarshaller_e - ticCreateUnmarshaller_b) / 1000.0 + "s");
 
             vec = new ValidationEventCollector();
             unmarshaller.setEventHandler(vec);
@@ -198,7 +204,8 @@ public class EPCPackage {
             long ticUnmarshall_b = System.currentTimeMillis();
             JAXBElement<?> result = (JAXBElement<?>) unmarshaller.unmarshal(bais);
             long ticUnmarshall_e = System.currentTimeMillis();
-            logger.debug("\t@parseXmlFromContext : Unmarshalling took " + (ticUnmarshall_e - ticUnmarshall_b) / 1000.0 + "s");
+            logger.debug("\t@parseXmlFromContext : Unmarshalling took " + (ticUnmarshall_e - ticUnmarshall_b) / 1000.0
+                    + "s");
 
             logger.debug("result  " + result);
             if (vec.getEvents().length > 0) {
@@ -208,19 +215,21 @@ public class EPCPackage {
                 logger.debug("events found");
                 return null;
             } else if (result != null) {
-                if(testRemarshalling) {
+                if (testRemarshalling) {
                     logger.debug("no events found");
                     long ticMarshall_b = System.currentTimeMillis();
                     String xmlNewContent = marshal(result.getValue());
                     long ticMarshall_e = System.currentTimeMillis();
-                    logger.debug("\t@parseXmlFromContext : Marshalling took " + (ticMarshall_e - ticMarshall_b) / 1000.0 + "s");
+                    logger.debug("\t@parseXmlFromContext : Marshalling took " + (ticMarshall_e - ticMarshall_b) / 1000.0
+                            + "s");
 
                     long ticCountingChevron_b = System.currentTimeMillis();
                     int countOld = 0;
                     int countNew = 0;
                     for (int i = 0; i < xmlNewContent.length(); i++)
                         if (xmlNewContent.charAt(i) == '<')
-                            if (xmlNewContent.charAt(i + 1) != '?') // On ne compte pas le <?xml du dÃ©part qui n'est pas
+                            if (xmlNewContent.charAt(i + 1) != '?') // On ne compte pas le <?xml du dÃ©part qui n'est
+                                                                    // pas
                                 // toujours la
                                 countNew++;
 
@@ -232,7 +241,8 @@ public class EPCPackage {
 
                     long ticCountingChevron_e = System.currentTimeMillis();
 
-                    logger.debug("\t@parseXmlFromContext : Counting chevron cost " + (ticCountingChevron_e - ticCountingChevron_b) / 1000.0 + "s");
+                    logger.debug("\t@parseXmlFromContext : Counting chevron cost "
+                            + (ticCountingChevron_e - ticCountingChevron_b) / 1000.0 + "s");
 
                     if (countOld > countNew) {
                         logger.error("Error reading object class : " + result.getValue().getClass().getName()
@@ -240,7 +250,8 @@ public class EPCPackage {
                         if (tryWithoutNamespaceIfFail) {
 
                             logger.debug("Trying without namespace");
-                            String contentNoNamespace = xmlContent.replaceAll("<(/?)[a-zA-Z0-9_]+:([\\w\\d_]+)", "<$1$2");
+                            String contentNoNamespace = xmlContent.replaceAll("<(/?)[a-zA-Z0-9_]+:([\\w\\d_]+)",
+                                    "<$1$2");
                             return parseXmlFromContext(contentNoNamespace, false, testRemarshalling, schema);
                         } else {
                             return null;
@@ -261,23 +272,25 @@ public class EPCPackage {
                 assert !processedXmlContent.contains(":obj_") && !processedXmlContent.contains("<obj_");
                 logger.debug("Applied obj_ prefix removal transformation");
                 try {
-                    JAXBElement<?> result = parseXmlFromContext(processedXmlContent, tryWithoutNamespaceIfFail, testRemarshalling, schema);
+                    JAXBElement<?> result = parseXmlFromContext(processedXmlContent, tryWithoutNamespaceIfFail,
+                            testRemarshalling, schema);
                     if (result != null) {
                         logger.debug("Successfully parsed after obj_ prefix removal");
                         return result;
                     }
-                } catch (Exception ignore) {}
+                } catch (Exception ignore) {
+                }
                 logger.debug("Failed to parse after obj_ prefix removal");
-            }else{
-                if(schema != null){
+            } else {
+                if (schema != null) {
                     logger.debug(e.getCause() + " " + e.getMessage());
                     // try to parse without schema
                     return parseXmlFromContext(xmlContent, tryWithoutNamespaceIfFail, testRemarshalling, null);
                 }
                 logger.debug(e.getMessage(), e);
                 logger.debug("File not read : ");
-                logger.debug(xmlContent.substring(0,500) + " [.....]");
-                if(vec != null){
+                logger.debug(xmlContent.substring(0, 500) + " [.....]");
+                if (vec != null) {
                     logger.debug("\tRead event [" + vec.getEvents().length + "]");
                     for (ValidationEvent ev : vec.getEvents()) {
                         logger.debug("event : " + ev);
@@ -301,7 +314,7 @@ public class EPCPackage {
                     JAXBElement<?> elt = wrap(obj, factory);
                     marshaller.marshal(elt, os);
                 } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
-                         | InvocationTargetException e) {
+                        | InvocationTargetException e) {
                     logger.error(e.getMessage(), e);
                 }
             } catch (JAXBException e) {
@@ -322,35 +335,35 @@ public class EPCPackage {
         methodPotentialName.add("create" + typename);
         methodPotentialName.add(typename.substring(0, 1).toLowerCase() + typename.substring(1));
         for (String prefixToRemove : listOfPotentialRemovablePrefix) {
-            if(typename.startsWith(prefixToRemove)) {
+            if (typename.startsWith(prefixToRemove)) {
                 String noPrefix = typename.substring(prefixToRemove.length());
                 methodPotentialName.add("create" + noPrefix);
                 methodPotentialName.add(noPrefix.substring(0, 1).toLowerCase() + noPrefix.substring(1));
             }
         }
 
-        for(String m_name : methodPotentialName){
+        for (String m_name : methodPotentialName) {
             try {
                 Method m = classFactory22.getMethod(m_name, class22);
                 return (JAXBElement<?>) m.invoke(factory, energymlObject);
-            }catch (NoSuchMethodException e){
+            } catch (NoSuchMethodException e) {
                 // pass
             } catch (Exception e2) {
                 logger.error(e2.getMessage(), e2);
             }
         }
         logger.error("No method found to create a type " + typename);
-        logger.error("Tested names " + methodPotentialName.stream().reduce((s1,s2) -> s1 + ", " + s2));
+        logger.error("Tested names " + methodPotentialName.stream().reduce((s1, s2) -> s1 + ", " + s2));
         return null;
     }
 
     public boolean validate(Object obj) throws JAXBException {
-        if(obj != null) {
+        if (obj != null) {
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             jaxbUnmarshaller.setSchema(xsdSchema);
 
             String xmlObj = marshal(obj);
-            System.out.println(xmlObj);
+            logger.debug(xmlObj);
             ByteArrayInputStream bais = new ByteArrayInputStream(xmlObj.getBytes(StandardCharsets.UTF_8));
 
             jaxbUnmarshaller.unmarshal(bais);
@@ -360,7 +373,7 @@ public class EPCPackage {
     }
 
     public String marshal(Object obj) {
-        if(obj != null) {
+        if (obj != null) {
             try {
                 OutputStream os = new ByteArrayOutputStream();
                 marshal(obj, os);
@@ -373,13 +386,13 @@ public class EPCPackage {
         return null;
     }
 
-/*
-       ______     __  __                    _______      __  __
-      / ____/__  / /_/ /____  __________  _/_/ ___/___  / /_/ /____  __________
-     / / __/ _ \/ __/ __/ _ \/ ___/ ___/_/_/ \__ \/ _ \/ __/ __/ _ \/ ___/ ___/
-    / /_/ /  __/ /_/ /_/  __/ /  (__  )/_/  ___/ /  __/ /_/ /_/  __/ /  (__  )
-    \____/\___/\__/\__/\___/_/  /____/_/   /____/\___/\__/\__/\___/_/  /____/
-*/
+    /*
+     * ______ __ __ _______ __ __
+     * / ____/__ / /_/ /____ __________ _/_/ ___/___ / /_/ /____ __________
+     * / / __/ _ \/ __/ __/ _ \/ ___/ ___/_/_/ \__ \/ _ \/ __/ __/ _ \/ ___/ ___/
+     * / /_/ / __/ /_/ /_/ __/ / (__ )/_/ ___/ / __/ /_/ /_/ __/ / (__ )
+     * \____/\___/\__/\__/\___/_/ /____/_/ /____/\___/\__/\__/\___/_/ /____/
+     */
 
     public String getDomain() {
         return domain;
@@ -409,7 +422,7 @@ public class EPCPackage {
         return devVersionNum;
     }
 
-    public boolean matchNamespace(String namespace){
+    public boolean matchNamespace(String namespace) {
         return namespace.toLowerCase().contains(domain.toLowerCase());
     }
 }
